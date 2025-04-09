89872f9a3e27d17df6ecdfabe65e58d298d898af
JamesSMV <smveugene@yahoo.com>
2025-04-09 11:38:57 +0300

    Add SSH SSH

--- repo_git.py
+++ repo_git.py
@@ -2,4 +2,4 @@
 # 2 один раз прошёл авторизацию через сайт и потом через токен push получился, теперь даже окно выбора не выводится
 # 3 echo "url=https://github.com" | git credential reject
 # 4 git push origin test_james_saucedemo
-# 5 SSH
+# 5 SSH SSH
