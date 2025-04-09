f36a3567dfee13c33195da8e3219ded6848b76af
JamesSMV <smveugene@yahoo.com>
2025-04-09 08:50:01 +0300

    Add repo_git 2git

--- repo_git.py
+++ repo_git.py
@@ -1,4 +1,4 @@
 # 1 черезToken не работает
 # 2 один раз прошёл авторизацию через сайт и потом через токен push получился, теперь даже окно выбора не выводится
-# 3
+# 3 echo "url=https://github.com" | git credential reject
 # 4 git push origin test_james_saucedemo
